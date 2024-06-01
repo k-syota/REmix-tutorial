d121170b2e8d3ee81438e63781f472c7cb2383d8
k-syota <syota4723@gmail.com>
2024-06-01 14:29:11 +0900

    💄 スタイルシートの追加

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction } from "@remix-run/node";
 import {
   Form,
   Links,
@@ -5,10 +6,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { appStyleHref } from "./app.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStyleHref }
+];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
